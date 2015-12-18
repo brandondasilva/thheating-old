7d4fd0cd71e653a80d5a00f3268ff5430ab30cc0
Brandon Da Silva <da.silva.brandon@gmail.com>
2015-12-17 22:21:32 -0500

    Deleted new site folder

--- NEWSITE/public/js/modules/template.js
+++
@@ -1,9 +0,0 @@
-define(['underscore', 'jquery'], function() {
-  var showName = function(n) {
-    var temp = _.template("Hello <%= name %>");
-    $(".new-class").html(temp({name: n}));
-  };
-  return {
-    showName: showName
-  };
-});
