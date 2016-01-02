b29d6613d49ad08f30390167175f6a98abf233e2
Brandon Da Silva <da.silva.brandon@gmail.com>
2016-01-01 20:09:41 -0500

    Initial commit of new website design

---
+++ thheating/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<?php include '_assets-custom/includes/_head.html'; ?>
+</head>
+
+<body>
+	
+
+	<!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="_assets-custom/js/materialize.min.js"></script>
+    <script type="text/javascript" src="_assets-custom/js/main.js"></script>
+</body>
+</html>
